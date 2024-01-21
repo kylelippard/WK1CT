bc6b62f9c611f60dbcf08ccfa0bbb79ac3acd41f
kylelippard <kylelippard@gmail.com>
2024-01-21 15:40:19 -0600

    Adding project to GitHub Repo

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		String firstName = "Sirius";
+		String lastName = "Black";
+		String address = "12 Grimmauld Place";
+		String city = "Islington, London";
+		String postalCode = "N1 2YL";
+		
+		System.out.println(firstName + "\n" + lastName + "\n" + address
+				+ "\n" + city  + "\n" + postalCode);
+
+	}
+}
